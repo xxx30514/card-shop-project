7491e7193534b378ed6041bad02780c377c4b4d3
xxx30514 <xxx30514@gmail.com>
2024-06-19 18:13:51 +0800

    6/19 第一次上傳

---
+++ src/main/java/com/myproject/cardshop/CardshopApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.cardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/cardshop/CardshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.cardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

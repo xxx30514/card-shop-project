329dd9c16626bbfbf27bf5e56dffcfec3c9f4804
xxx30514 <xxx30514@gmail.com>
2024-06-19 18:31:58 +0800

    test2

---
+++ src/main/java/com/myproject/cardshop/pojo/Order.java
@@ -0,0 +1,26 @@
+package com.myproject.cardshop.pojo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+//import com.baomidou.mybatisplus.annotation.TableId;
+//import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+@Data
+//@TableName("t_order")
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	//@TableId
+	private Integer orderId;
+	private Long orderNum;
+	private Integer userId;
+	private Integer productId;
+	private Integer productNum;
+	private BigDecimal productPrice;
+	private LocalDateTime orderTime;
+	private Integer addressId;
+
+}

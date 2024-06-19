610368c5ecf5b6c104a14677b22da90b098566c1
xxx30514 <xxx30514@gmail.com>
2024-06-19 18:28:18 +0800

    test

---
+++ src/main/java/com/myproject/cardshop/config/FilePathConfig.java
@@ -0,0 +1,13 @@
+package com.myproject.cardshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class FilePathConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/image/**").addResourceLocations("file:C:/myproject/testimg/");
+	}
+}
---
+++ src/main/java/com/myproject/cardshop/config/RabbitMqConfig.java
@@ -0,0 +1,19 @@
+package com.myproject.cardshop.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+	/**
+	 * RabbitMQ序列化方式 Json 格式
+	 * @return
+	 */
+	@Bean
+	public MessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+}

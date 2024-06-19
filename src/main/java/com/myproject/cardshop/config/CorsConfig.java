4676e5c2668c9e5cf9aa4a873bc5b5adc710e985
xxx30514 <xxx30514@gmail.com>
2024-06-19 18:25:35 +0800

    test2

---
+++ src/main/java/com/myproject/cardshop/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.myproject.cardshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    //
+    registry.addMapping("/**")
+        // 是否發送cookie
+        .allowCredentials(true)
+        // 放行原始域名
+        .allowedOriginPatterns("*")
+        // 放行的請求方式
+        .allowedMethods("*")
+        // 放行那些HTTP header
+        .allowedHeaders("*").exposedHeaders("*");
+  }
+}

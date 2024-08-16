2a85fa16a8cc77303095f46c061e1b7aa5d9853d
xxx30514 <xxx30514@gmail.com>
2024-08-17 03:25:32 +0800

    8/17 derived query

--- src/main/java/com/myproject/cardshop/repositories/AuthorRepository.java
+++ src/main/java/com/myproject/cardshop/repositories/AuthorRepository.java
@@ -1,9 +1,26 @@
 package com.myproject.cardshop.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.myproject.cardshop.entities.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer>{
-
+	// derived query
+	// 此方法相當於原生SQL的 select * from author where first_name = '${firstName}'
+	List<Author> findAllByFirstName(String firstName);
+	
+	// 忽略大小寫 ex.JASON&Jason IgnoreCase || IgnoringCase 皆可 僅是命名風格差異
+	List<Author> findAllByFirstNameIgnoreCase(String firstName);
+	
+	// 此方法相當於原生SQL的 select * from author where first_name like '%${firstName}%'
+	List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+	
+	// select * from author where first_name like '${firstName}%'
+	List<Author> findAllByFirstNameStartsWithIgnoreCase(String firstName);
+	
+	// select * from author where first_name like '%${firstName}'
+	List<Author> findAllByFirstNameEndsWithIgnoreCase(String firstName);
+	
+	//  select * from author where first_name in ('','','')
+	List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 }

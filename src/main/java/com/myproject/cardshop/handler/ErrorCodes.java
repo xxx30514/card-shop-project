635e41d34868a00f00e4d13e503f569379a916e1
xxx30514 <xxx30514@gmail.com>
2025-04-18 01:28:32 +0800

    exceptionHandler 增加新的例外情況與錯誤碼

--- src/main/java/com/myproject/cardshop/handler/ErrorCodes.java
+++ src/main/java/com/myproject/cardshop/handler/ErrorCodes.java
@@ -5,18 +5,23 @@ import org.springframework.http.HttpStatus;
 import lombok.Getter;
 
 public enum ErrorCodes {
-	
+
 	NO_CODE(0, HttpStatus.NOT_IMPLEMENTED, "NO code"),
-	
-	INCORRECT_CURRENT_PASSWORD(300,HttpStatus.BAD_REQUEST,"密碼錯誤"),
-	
-	NEW_PASSWORD_DOES_NOT_MATCT(301,HttpStatus.BAD_REQUEST,"密碼錯誤"),
-	
+
+	INCORRECT_CURRENT_PASSWORD(300, HttpStatus.BAD_REQUEST, "密碼錯誤"),
+
+	NEW_PASSWORD_DOES_NOT_MATCH(301, HttpStatus.BAD_REQUEST, "密碼錯誤"),
+
 	ACCOUNT_LOCKED(302, HttpStatus.FORBIDDEN, "您的帳號已被鎖定，請聯絡管理員"),
-	
+
 	ACCOUNT_DISABLED(303, HttpStatus.FORBIDDEN, "您的帳號尚未啟用，請進行email驗證"),
+
+	BAD_CREDENTIALS(304, HttpStatus.UNAUTHORIZED, "帳號或密碼錯誤，請重新輸入"),
+
+	EMAIL_SENDING_FAILED(305, HttpStatus.INTERNAL_SERVER_ERROR, "信件發送失敗，請稍後再試"),
 	
-	BAD_CREDENTIALS(304, HttpStatus.UNAUTHORIZED, "帳號或密碼錯誤，請重新輸入");
+	INTERNAL_ERROR(306, HttpStatus.INTERNAL_SERVER_ERROR, "發生未知錯誤，請稍後再試");
+	
 
 	@Getter
 	private final int code;
--- src/main/java/com/myproject/cardshop/handler/GlobalExceptionHandler.java
+++ src/main/java/com/myproject/cardshop/handler/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.myproject.cardshop.handler;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import jakarta.mail.MessagingException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,7 +31,28 @@ public class GlobalExceptionHandler {
 		return buildExceptionResponse(ErrorCodes.BAD_CREDENTIALS, exception);
 	}
 
-	// 公共方法處理錯誤響應 減少重複程式碼
+	@ExceptionHandler(MessagingException.class)
+	public ResponseEntity<ExceptionResponse> handleMessagingException(MessagingException exception) {
+		return buildExceptionResponse(ErrorCodes.EMAIL_SENDING_FAILED, exception);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		Set<String> errors = new HashSet<>(); 
+		exception.getBindingResult().getAllErrors().forEach(error -> {
+			String errorMessage = error.getDefaultMessage();
+			errors.add(errorMessage);
+		});
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionResponse.builder().validationErrors(errors).build());
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+		exception.printStackTrace();
+		return buildExceptionResponse(ErrorCodes.INTERNAL_ERROR, exception);
+	}
+
+	// 公共方法處理多數錯誤響應 減少重複程式碼
 	private ResponseEntity<ExceptionResponse> buildExceptionResponse(ErrorCodes errorCode, Exception exception) {
 		return ResponseEntity.status(errorCode.getHttpStatus())
 				.body(ExceptionResponse.builder().errorCode(errorCode.getCode())

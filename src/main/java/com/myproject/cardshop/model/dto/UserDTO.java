dbdd4a7dc4f7537618d249e96ee2ab5c4a2655d3
xxx30514 <xxx30514@gmail.com>
2025-08-28 20:21:30 +0800

    8/28 DTO轉換改成builder寫法

--- src/main/java/com/myproject/cardshop/model/dto/UserDTO.java
+++ src/main/java/com/myproject/cardshop/model/dto/UserDTO.java
@@ -2,12 +2,14 @@ package com.myproject.cardshop.model.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
--- src/main/java/com/myproject/cardshop/model/mapper/UserMapper.java
+++ src/main/java/com/myproject/cardshop/model/mapper/UserMapper.java
@@ -8,9 +8,14 @@ import com.myproject.cardshop.model.dto.UserDTO;
 @Service
 public class UserMapper {
 
+//	public UserDTO toDto(User user) {
+//		List<String> roleNames = user.getRoles().stream().map(role -> role.getName()).toList();
+//		return new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), roleNames,
+//				user.getFullName());
+//	}
 	public UserDTO toDto(User user) {
 		List<String> roleNames = user.getRoles().stream().map(role -> role.getName()).toList();
-		return new UserDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), roleNames,
-				user.getFullName());
+		return UserDTO.builder().id(user.getId()).firstName(user.getFirstName()).lastName(user.getLastName())
+				.email(user.getEmail()).roles(roleNames).fullName(user.getFullName()).build();
 	}
 }
